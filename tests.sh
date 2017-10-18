d72b3b3e08b02fc5c02be6af9a8c0b5fa330f720
Rodolfo Martínez <rodolfo@agavelab.com>
2017-10-17 21:27:30 -0500

    Añadido configuración para CircleCI

---
+++ tests.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+python manage.py migrate personas zero
+python manage.py migrate lugares zero
+python manage.py migrate users zero
+python manage.py makemigrations
+python manage.py migrate
